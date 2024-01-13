22c767484912a5c205092bc5ba757bb855930a54
Raul GM <loscanos3raul@gmail.com>
2024-01-13 19:20:19 +0100

    local storage del usuario

--- js/navegacion/index_nav.js
+++ js/navegacion/index_nav.js
@@ -2,15 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let iniciar = document.getElementById('ini')
     let registrarse = document.getElementById('regis')
-    let nick = document.getElementById('nick').value
     
     iniciar.addEventListener('click',inicio)
     registrarse.addEventListener('click',registro)
 
     function inicio() {
+        
+        let nick = document.getElementById('nick').value
         localStorage.clear('nicks')
         localStorage.setItem('nicks','nick')
-        window.location.href = "../../html/inicio.html"
+        console.log(nick)
+        //window.location.href = "../../html/inicio.html"
     }
 
     function registro(){
